e61125379973ddb3224a23db34592e286b93f244
Abubakr Ghaznavi <AGhaznavi@sandstonetechnologies.com>
2022-05-11 10:30:32 -0400

    Added code to test for copper link in Accedian Switch

---
+++ MetronodeLTCopperLinkTester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace AutoTest
+{
+    class MetronodeLTCopperLinkTester
+    {
+        public const int PORT_COUNT = 12;
+        public static bool[] GetStatuses(String html)
+        {
+            bool[] linked = new bool[PORT_COUNT];
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+            HtmlNodeCollection nodeCollection = document.DocumentNode.SelectNodes("//td/img");
+            int portIdx = 0;
+            foreach (HtmlNode nodeElem in nodeCollection)
+            {
+                // If the port index represents a non SFP port then we have iterated over all SFP ports
+                if (portIdx >= PORT_COUNT) { break; }
+                string bubbleImage = (nodeElem.GetAttributeValue("src", "null"));
+                if (bubbleImage.Equals("green.gif"))
+                {
+                    linked[portIdx] = true;
+                }
+                else
+                {
+                    linked[portIdx] = false;
+                }
+                portIdx += 1;
+            }
+            return linked;
+        }
+    }
+}
